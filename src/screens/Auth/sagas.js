da1c0fbd4e864f3da38213524b3856b13bcf3805
Ahmad Najam Azizi <ahmad_azizi@live.com>
2020-07-28 17:44:46 +0500

    changes

--- src/screens/Auth/sagas.js
+++ src/screens/Auth/sagas.js
@@ -9,9 +9,9 @@ function* onSignup({ signup }, { body }) {
     const response = yield call(signup, body);
 
     if (response.ok) {
-        alert('Registered successfully');
         yield put({ type: AuthTypes.SET_SIGNUP_SUCCESSFUL, signupSuccessful: true });
         yield put({ type: AuthTypes.SET_IS_LOADING, isLoading: false });
+        alert('Registered successfully, please login');
     } else {
         alert(response.data.message);
         yield put({ type: AuthTypes.SET_IS_LOADING, isLoading: false });
@@ -27,8 +27,8 @@ function* onLogin({ login }, { body }) {
         yield put({ type: AuthTypes.SET_IS_USER_LOGGED_IN, isUserLoggedIn: true });
         yield put({ type: AuthTypes.SET_IS_LOADING, isLoading: false });
     } else {
-        alert(response.data.message);
         yield put({ type: AuthTypes.SET_IS_LOADING, isLoading: false });
+        alert(response.data.message);
     }
 }
 
@@ -38,11 +38,11 @@ function* onUpdatePassword({ updatePassword }, { body }) {
 
     if (response.ok) {
         yield put({ type: AuthTypes.ON_LOGOUT });
+        yield put({ type: AuthTypes.SET_IS_LOADING, isLoading: false });
         alert('Updated password successfully, please login.');
-        yield put({ type: AuthTypes.SET_IS_LOADING, isLoading: false });
     } else {
-        alert(response.data.message);
         yield put({ type: AuthTypes.SET_IS_LOADING, isLoading: false });
+        alert(response.data.message);
     }
 }
 
